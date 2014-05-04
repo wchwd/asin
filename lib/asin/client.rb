3bd3426cf033ddd508d604aabcb8e81185483a23
Michael Feldstein <msfeldstein@gmail.com>
2014-05-04 12:32:23 -0700

    Don't use httpi, it breaks the signature for some reason

--- lib/asin/client.rb
+++ lib/asin/client.rb
@@ -252,7 +252,7 @@ module ASIN
       url = "http://#{Configuration.host}#{PATH}?#{signed}"
       log(:info, "performing rest call to url='#{url}'")
 
-      response = HTTPI.get(url)
+      response = Net::HTTP.get(URI.parse(url))
       if response.code == 200
         # force utf-8 chars, works only on 1.9 string
         resp = response.body
